8f11a847accd03a31fd333c15120890364647ca9
unknow <138153020+ff6f8d68@users.noreply.github.com>
2025-02-15 12:49:48 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+GITPAK_DIR="$HOME/.gitpak"
+INSTALL_DIR="$GITPAK_DIR/packages"
+mkdir -p "$GITPAK_DIR"
+temp_gitpak=$(mktemp)
+curl -sL "https://raw.githubusercontent.com/I-FOX-Development/gitpak/refs/heads/main/gitpak.sh" -o "$temp_gitpak"
+mv "$temp_gitpak" "$GITPAK_DIR/gitpak.sh"
+chmod +x "$GITPAK_DIR/gitpak.sh"
+echo "export PATH=\"$GITPAK_DIR:$PATH\"" >> ~/.bashrc
+echo "export PATH=\"$GITPAK_DIR:$PATH\"" >> ~/.zshrc
+source ~/.bashrc
+source ~/.zshrc
+ln -sf "$GITPAK_DIR/gitpak.sh" "$GITPAK_DIR/gitpak"
+source ~/.bashrc
+echo "Gitpak installed successfully."
